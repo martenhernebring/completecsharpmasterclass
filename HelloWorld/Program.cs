13ce2aa4855834f151099a1cb48bf7e35523632d
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-19 12:45:15 +0100

    Parse int to string

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,13 +6,11 @@ namespace HelloWorld
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("Enter a string a press enter:");
-			string readInput = Console.ReadLine();
-			Console.WriteLine("You have entered {0}", readInput);
-			Console.Write("Enter any key and press enter");
-			int asciiValue = Console.Read();
-			Console.WriteLine("ASCII value is {0}", asciiValue);
-			Console.ReadKey();
+			string myString = "15d";
+			string mySecondString = "13";
+			int num1 = Int32.Parse(myString);
+			int num2 = Int32.Parse(mySecondString);
+			Console.WriteLine(num1 + num2);
 		}
 	}
 }
