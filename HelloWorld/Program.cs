de243c2b169925d74600bdc8a754c89b28ebb348
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-19 12:24:33 +0100

    console ascii app; readme

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter a string a press enter:");
+			string readInput = Console.ReadLine();
+			Console.WriteLine("You have entered {0}", readInput);
+			Console.Write("Enter any key and press enter");
+			int asciiValue = Console.Read();
+			Console.WriteLine("ASCII value is {0}", asciiValue);
+			Console.ReadKey();
+		}
+	}
+}
