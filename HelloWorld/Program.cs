4d24e5f5fcfe4d9110a4924014a73eb0cb33543c
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-20 12:33:31 +0100

    Try catch divide by zero

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,16 +6,21 @@ namespace HelloWorld
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("Enter factor one: ");
-			string factor1 = Console.ReadLine();
-			Console.Write("Enter factor two: ");
-			string factor2 = Console.ReadLine();
-			Console.WriteLine("The sum is {0}", Add(factor1, factor2));
+			int dividend = 1;
+			int divisor = 0;
+			try
+			{
+				int divideByZero = dividend / divisor;
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("Internal error: Divide by zero");
+			}
+			finally
+			{
+				//close open connections
+			}
 		}
 
-		private static int Add(string factor1, string factor2)
-		{
-			return Int32.Parse(factor1) + Int32.Parse(factor2);
-		}
 	}
 }
