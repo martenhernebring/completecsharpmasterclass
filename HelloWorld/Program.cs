b19b4cd7c154e390b44ff4456b1d52ae9137fec5
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-20 10:14:35 +0100

    How to create constants

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,42 +1,16 @@
 ﻿using System;
-using System.Globalization;
 
 namespace HelloWorld
 {
 	internal class Program
 	{
+		const double Pi = 3.14159265359;
+		const int Weeks = 52, Months = 12;
+		const string BirthDay = "1986-04-26";
+
 		static void Main(string[] args)
 		{
-			byte ub = 4;
-			sbyte sb = -1;
-			ushort us = 5000;
-			short ss = -300;
-			uint ui = 80000;
-			int si = -80000;
-			ulong ul = 5000000000;
-			long sl = -5000000000;
-			float fpi = 3.14F;
-			double dpi = 3.1415927;
-			char letter = 'A';
-			bool dayLight = false;
-			decimal cents50 = 0.50M;
-			string text = "I control text";
-			string wholeNumber = "15";
-			int number = Int32.Parse(wholeNumber);
-			Console.WriteLine(ub);
-			Console.WriteLine(sb);
-			Console.WriteLine(us);
-			Console.WriteLine(ss);
-			Console.WriteLine(ui);
-			Console.WriteLine(si);
-			Console.WriteLine(ul);
-			Console.WriteLine(sl);
-			Console.WriteLine(fpi);
-			Console.WriteLine(dpi);
-			Console.WriteLine(letter);
-			Console.WriteLine(dayLight);
-			Console.WriteLine(cents50);
-			Console.WriteLine(number);
+			Console.WriteLine("My birthday is always going to be: {0}", BirthDay);
 		}
 	}
 }
