0e15af842913b295cb8e601f013de4ec2c4027d5
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-21 11:21:10 +0100

    Created a register and login program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,25 +4,56 @@ namespace HelloWorld
 {
 	internal class Program
 	{
+		private static string _registeredUser;
+		private static string _registeredPassword;
+
 		static void Main(string[] args)
+		{
+			RegisterUser();
+			string result = Login();
+			if (result.Equals(""))
+
+				Greet(_registeredUser);
+
+			else
+
+				Console.WriteLine(result);
+
+		}
+
+		private static void RegisterUser()
 		{
 			Console.WriteLine("Register new user.");
 			Console.Write("Please enter your new username: ");
-			string registeredUser = Console.ReadLine();
+			_registeredUser = Console.ReadLine();
 			Console.Write("Please enter your new password: ");
-			string registeredPassword = Console.ReadLine();
+			_registeredPassword = Console.ReadLine();
+			Console.WriteLine("Registration completed");
+			Console.WriteLine("-------------------------------------------");
+		}
+
+		private static string Login()
+		{
 			Console.WriteLine("Login");
 			Console.Write("Please enter your username: ");
 			string userName = Console.ReadLine();
-			if (userName.Equals(registeredUser))
+			if (userName.Equals(_registeredUser))
 			{
 				Console.Write("Please enter your password: ");
 				string userPassword = Console.ReadLine();
-				if (userPassword.Equals(registeredPassword))
-				
-					Greet(userName);
-				
+				if (userPassword.Equals(_registeredPassword))
+
+					return "";
+
+				else
+
+					return "Login failed, wrong password. Restart program";
+
 			}
+			else
+
+				return "Login failed, wrong username. Restart program";
+
 		}
 
 		private static void Greet(string userName)
