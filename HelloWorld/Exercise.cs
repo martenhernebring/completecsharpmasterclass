f531523647d50483b4395e8e6cbcbdbfbe24a644
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-21 10:45:17 +0100

    Finished exercise and made program to login

--- HelloWorld/Exercise.cs
+++ HelloWorld/Exercise.cs
@@ -4,16 +4,32 @@ namespace Coding.Exercise
 {
 	public class Exercise
 	{
+		public static void NestedCheck(int number)
+		{
+			if (number % 3 == 0)
+			
+				Console.WriteLine("Divisible by 3.");
+			
+			else if (number % 7 == 0)
+			
+				Console.WriteLine("Divisible by 7.");
+			
+			else 
+			
+				Check(number);
+			
+		}
+
 		public static void Check(int number)
 		{
 			if (number % 2 == 0)
-			{
-				Console.WriteLine("Even");
-			}
+			
+				Console.WriteLine("Even number.");
+			
 			else
-			{
-				Console.WriteLine("Odd");
-			}
+			
+				Console.WriteLine("Odd number.");
+			
 		}
 	}
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,29 +6,40 @@ namespace HelloWorld
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Register new user.");
+			Console.Write("Please enter your new username: ");
+			string registeredUser = Console.ReadLine();
+			Console.Write("Please enter your new password: ");
+			string registeredPassword = Console.ReadLine();
+			Console.WriteLine("Login");
 			Console.Write("Please enter your username: ");
 			string userName = Console.ReadLine();
-			if (IsUserRegistered())
+			if (userName.Equals(registeredUser))
 			{
-				Console.Write("Hi there, ");
-				if (userName.ToLower().Equals("admin"))
+				Console.Write("Please enter your password: ");
+				string userPassword = Console.ReadLine();
+				if (userPassword.Equals(registeredPassword))
 				
-					Console.WriteLine("admin user");
-					
-				else if(userName != "")
-
-					Console.WriteLine(userName);
-
-				else
-
-					Console.WriteLine("registered user");
-
+					Greet(userName);
+				
 			}
 		}
 
-		private static bool IsUserRegistered()
+		private static void Greet(string userName)
 		{
-			return true;
+			Console.Write("Hi there, ");
+			if (userName.ToLower().Equals("admin"))
+
+				Console.WriteLine("admin user");
+
+			else if (userName != "")
+
+				Console.WriteLine(userName);
+
+			else
+
+				Console.WriteLine("registered user");
+
 		}
 	}
 }
