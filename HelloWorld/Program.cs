5e9d511624772268ace0082cbfd2256114194138
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-22 15:57:28 +0100

    Switch statement

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,73 +4,35 @@ namespace HelloWorld
 {
 	internal class Program
 	{
-		private static string _registeredUser;
-		private static string _registeredPassword;
 
 		static void Main(string[] args)
 		{
-			RegisterUser();
-			string result = Login();
-			if (result.Equals(""))
-
-				Greet(_registeredUser);
-
-			else
-
-				Console.WriteLine(result);
-
-		}
-
-		private static void RegisterUser()
-		{
-			Console.WriteLine("Register new user.");
-			Console.Write("Please enter your new username: ");
-			_registeredUser = Console.ReadLine();
-			Console.Write("Please enter your new password: ");
-			_registeredPassword = Console.ReadLine();
-			Console.WriteLine("Registration completed");
-			Console.WriteLine("-------------------------------------------");
-		}
-
-		private static string Login()
-		{
-			Console.WriteLine("Login");
-			Console.Write("Please enter your username: ");
-			string userName = Console.ReadLine();
-			if (userName.Equals(_registeredUser))
+			int age = 15;
+			switch (age)
 			{
-				Console.Write("Please enter your password: ");
-				string userPassword = Console.ReadLine();
-				if (userPassword.Equals(_registeredPassword))
-
-					return "";
-
-				else
-
-					return "Login failed, wrong password. Restart program";
-
+				case 15:
+					Console.WriteLine("Too young to party in the club");
+					break;
+				case 25:
+					Console.WriteLine("Good to go");
+					break;
+				default:
+					Console.WriteLine("How old are you then?");
+					break;
 			}
-			else
-
-				return "Login failed, wrong username. Restart program";
-
-		}
-
-		private static void Greet(string userName)
-		{
-			Console.Write("Hi there, ");
-			if (userName.ToLower().Equals("admin"))
-
-				Console.WriteLine("admin user");
-
-			else if (userName != "")
-
-				Console.WriteLine(userName);
-
-			else
-
-				Console.WriteLine("registered user");
-
+			string userName = "Morten";
+			switch (userName)
+			{
+				case "Morten":
+					Console.WriteLine("User name is Morten");
+					break;
+				case "root":
+					Console.WriteLine("User name is root");
+					break;
+				default:
+					Console.WriteLine("User name is unknown);
+					break;
+			}
 		}
 	}
 }
