d40dd8585e8258aeafcf1aa7a8582fc77234bf34
Marten Hernebring <marten.hernebring@gmail.com>
2022-12-26 11:45:49 +0100

    Check if entered type is correct or not

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,11 +7,46 @@ namespace HelloWorld
 
 		static void Main(string[] args)
 		{
-			string[] friends = { "f1", "f2", "f3", "f4", "f5" };
-			foreach (string friend in friends) 
+			Console.Write("Enter a string, integer or boolean: ");
+			string input = Console.ReadLine();
+			Console.WriteLine("You have entered a value: " + input);
+			Console.WriteLine("Press 1 for String, 2 for Integer, 3 for Boolean");
+			char key = Console.ReadKey().KeyChar;
+			Console.WriteLine();
+			bool valid = false;
+			string type = "";
+			switch (key) 
 			{
-				Console.WriteLine("Hi {0}", friend);
+				case '1':
+					type = "String";
+					valid = IsAllAlphabetic(input);
+					break;
+				case '2':
+					type = "Integer";
+					valid = int.TryParse(input, out int _);
+					break;
+				case '3':
+					type = "Boolean";
+					valid = bool.TryParse(input, out bool _);
+					break;
+				default:
+					Console.WriteLine("Only enter a number 1 - 3 next time!");
+					Environment.Exit(22);
+					break;
 			}
+			Console.Write("It is {0} : {1}", valid ? "valid" : "invalid", type);
+		}
+
+		private static bool IsAllAlphabetic(string input)
+		{
+			foreach (char ch in input)
+			{
+				if (!char.IsLetter(ch))
+				{
+					return false;
+				}
+			}
+			return true;
 		}
 	}
 }
