87278ef79d378b1af3e3d551ec72740a5cb3c9a5
Marten Hernebring <marten.hernebring@gmail.com>
2023-01-05 14:05:43 +0100

    Created a dictionary with converts int to string

---
+++ HelloWorld/Speller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+	public class Speller
+	{
+
+		public static string Convert(int i)
+		{
+			Dictionary<int, string> numberWords = new Dictionary<int, string>()
+			{
+				{ 0, "zero"},
+				{ 1, "one"},
+				{ 2, "two"},
+				{ 3, "three"},
+				{ 4, "four"},
+				{ 5, "five"}
+			};
+			string word;
+			if (numberWords.TryGetValue(i, out word))
+				return word;
+			else
+				return "nope";
+		}
+	}
+}
